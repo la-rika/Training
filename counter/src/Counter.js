5e450eae6510c2ef675f8b806c1c905c379531d4
Lara Forlani <larikaaa@Host-019.home>
2025-02-17 16:16:05 +0100

    project setup

---
+++ counter/src/Counter.js
@@ -0,0 +1,14 @@
+
+
+export const Counter = ()=>{
+
+ const [first, setfirst] = useState(second)
+
+    return(
+        <div>
+            <button type="button" onClick={}>-</button>
+            <p></p>
+            <button type="button" onClick={}>+</button>
+        </div>
+    )
+}
